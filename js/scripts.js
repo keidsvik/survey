70cfae01b33d3d7a647c25a381ccea09741ae035
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-25 14:18:18 -0700

    fix scripts.js to be able to log the data from the survey into the console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-var sign = $("#sign").val();
-
-var food = $("input:radio[name=food]:checked").val();
-
-var favoriteColor = $("#color").val();
-
-
 
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-   var survey = $("input#survey").val();
-  
     event.preventDefault();
+    var sign = $("#sign").val();
+    var food = $("input:radio[name=food]:checked").val();
+    var color = $("#fear").val();
+    console.log("sign:", sign, "food:", food, "color:", color)
+
+  //  var survey = $("#survey").select();
+  //  alert(survey);
+  
   });
 });
