bf3cca09d957aad1ff9d126dafe6ffda8a47a417
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-25 13:48:32 -0700

    add more questions and light css to survey page. fix color input type and radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ var sign = $("#sign").val();
 var food = $("input:radio[name=food]:checked").val();
 
 var favoriteColor = $("#color").val();
+
+
+
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+   var survey = $("input#survey").val();
+  
+    event.preventDefault();
+  });
+});
